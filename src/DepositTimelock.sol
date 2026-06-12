87d1ef0f0da1f083ec03e7ec9dceaf9db389c7b3
Ivan A. Sergeev <ivan@permianlabs.xyz>
2026-06-11 22:46:29 -0500

    src: update DepositTimelock version to v1.2

--- src/DepositTimelock.sol
+++ src/DepositTimelock.sol
@@ -32,7 +32,7 @@ contract DepositTimelock is
     /**
      * @notice Implementation version
      */
-    string public constant IMPLEMENTATION_VERSION = "1.1";
+    string public constant IMPLEMENTATION_VERSION = "1.2";
 
     /**
      * @notice Deposits storage location
